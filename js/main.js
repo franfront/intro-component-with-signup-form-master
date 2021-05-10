520f953af11c2be0320f73beae004c9d7c2cdd55
Maestro <fernandezfrancoa2001@gmail.com>
2021-05-09 23:13:48 -0300

    js

--- js/main.js
+++ js/main.js
@@ -1,17 +1,33 @@
 const form = document.getElementById("form");
+const inputs = form.querySelectorAll(".form-field")
 const emailRegex =  /^([a-z0-9_\.-]+)@([a-z\.]+)\.([a-z]{2,6})/;
 
+
+
 const validateForm = () =>{
-    if(form.name.value === ""){
-        form.name.parentElement.classList.add("form-field--error");
-    }else{
-        form.name.parentElement.classList.remove("form-field--error")
+    inputs.forEach(input =>{
+        if(
+            input.firstElementChild.value.trim() === "" &&
+            input.firstElementChild.name === "email"
+        ){
+            input.classList.add("form-field--error");
+        }else {
+            input.classList.remove("form-field--error")
+        }
+    });
+
+    if(!emailRegex.test(form.email.value.toLowerCase().trim())) {
+        form.email.parentElement.classList.add("form-field--error");
+    } else{
+        form.email.parentElement.classList.remove("form-field--error");
     }
 
-}
+   
+
+};
 
 
 form.addEventListener("submit", e =>{
     e.preventDefault();
     validateForm();
-})
+});
